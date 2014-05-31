f181dbc432d751700d2d7ea2b7e40cb5d7758353
Matt Boes <boes.matt@gmail.com>
2014-05-31 14:10:25 -0700

    Made test more generic with argument matchers.

--- core/src/test/java/com/boes/guideproject/core/test/DebugGuideServiceTest.java
+++ core/src/test/java/com/boes/guideproject/core/test/DebugGuideServiceTest.java
@@ -7,15 +7,20 @@ import com.boes.guideproject.core.GuideService;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+
 public class DebugGuideServiceTest {
 
     private final GuideListener guideListener = Mockito.mock(GuideListener.class);
     private final GuideService db = new DebugGuideService(guideListener);
-
     @Test
     public void notifiesGuideListenerWhenGuideReceived() {
         db.getGuideWithId("123");
-        Mockito.verify(guideListener).addPlace(0, "San Francisco", 37.7833, 122.4167);
+
+        // e.g. guideListener.addPlace(0, "San Francisco", 37.7833, 122.4167)
+        Mockito.verify(guideListener).addPlace(anyInt(), anyString(), anyDouble(), anyDouble());
     }
 
 }
